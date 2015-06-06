631fd64baf023a83e425706b346fc1fdf4d891b2
mambax7 <mambax7@gmail.com>
2015-06-06 07:51:47 -0400

    Original files

---
+++ ixt01603_v2/xoplugins/footer_custom_block01.php
@@ -0,0 +1,33 @@
+<?php
+/*
+* Qualitative Themes for XOOPS
+*
+* @license       http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+* @project       IXThemes Project, http://ixthemes.org
+* @theme_name    ixt01603
+* @theme_demo    http://ixthemes.sourceforge.net/demo/x/index.php?xoops_theme_select=ixt01603
+* @theme_author  Alexander Galochkin (algalochkin at users.sourceforge.net)
+* @version       $Id: footer_custom_block01.php 1731 2009-12-08 14:43:28Z algalochkin $
+*
+* Thanks you, that you have left this header untouched!!!
+*/
+$block = array(
+	'id'        => '1001',
+	'title'     => 'Custom Footer',
+	'content'   => '
+	<div>
+	Hello!
+	<br/>
+	Here the custom footer block 01.
+	<blockquote>
+	<p>
+	&ldquo;IXThemes - Suit For Your Content&rdquo;
+	<br />
+	&ldquo;XOOPS - Powered by you!&rdquo;
+	</p>
+	</blockquote>
+	</div>
+	',
+);
+$this->assign('fblock', $block);
+?>
---
+++ ixt01603_v2/xoplugins/footer_system_block11.php
@@ -0,0 +1,54 @@
+<?php
+/*
+* Qualitative Themes for XOOPS
+*
+* @license       http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+* @project       IXThemes Project, http://ixthemes.org
+* @theme_name    ixt01603
+* @theme_demo    http://ixthemes.sourceforge.net/demo/x/index.php?xoops_theme_select=ixt01603
+* @theme_author  Alexander Galochkin (algalochkin at users.sourceforge.net)
+* @version       $Id: footer_system_block11.php 1731 2009-12-08 14:43:28Z algalochkin $
+*
+* Thanks you, that you have left this header untouched!!!
+*/
+/*
+* system blocks id & templates:
+*  1 - system_block_user.html
+*  2 - system_block_login.html
+*  3 - system_block_search.html
+*  4 - system_block_waiting.html
+*  5 - system_block_mainmenu.html
+*  6 - system_block_siteinfo.html
+*  7 - system_block_online.html
+*  8 - system_block_topusers.html
+*  9 - system_block_newusers.html
+* 10 - system_block_comments.html
+* 11 - system_block_notification.html
+* 12 - system_block_themes.html
+*/
+
+include_once XOOPS_ROOT_PATH.'/class/theme_blocks.php';
+include_once XOOPS_ROOT_PATH.'/class/template.php';
+
+	$xobject = new XoopsBlock(11);
+	$template = new XoopsTpl();
+	$block = array(
+					'id'        => $xobject->getVar( 'bid' ),
+					'module'    => $xobject->getVar( 'dirname' ),
+					'title'     => $xobject->getVar( 'title' ),
+					//'name'        => strtolower( preg_replace( '/[^0-9a-zA-Z_]/', '', str_replace( ' ', '_', $xobject->getVar( 'name' ) ) ) ),
+					'weight'    => $xobject->getVar( 'weight' ),
+					'lastmod'   => $xobject->getVar( 'last_modified' ),
+	);
+	
+	$tplName = ( $tplName = $xobject->getVar('template') ) ? "db:$tplName" : "db:system_block_dummy.html";
+	if ( $bresult = $xobject->buildBlock() ) {
+					$template->assign( 'block', $bresult );
+					$block['content'] = $template->fetch($tplName);
+	} else {
+					$block = false;
+	}
+	
+	$this->assign('fblock', $block);
+
+?>
